ed7eb0ba0e1a6ddc798bc28a7377369b96c2720e
Parker H <geodasher10@gmail.com>
2026-03-30 19:02:50 -0500

    Add project files.

---
+++ PrimitiveShapeBuilder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimitiveShapeBuilder
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
